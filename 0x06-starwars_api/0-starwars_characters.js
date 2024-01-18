ed32051744bccc481ca692926e17424d2fac6e0e
Paul John <rainson.work@gmail.com>
2024-01-18 16:11:56 +0300

    0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieID = process.argv[2];
+const movieURL = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+function sendRequest (characterList, index) {
+  if (characterList.length == index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
+}
+
+request(movieURL, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
+  }
+});
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,17 @@
+## 0x06. Star Wars API
+### Star Wars Movie Character Printer
+
+Write a script that prints all characters of a Star Wars movie:
+
+- The first positional argument passed is the Movie ID - example: 3 = "Return of the Jedi"
+- Display one character name per line in the same order as the "characters" list in the `/films/` endpoint
+- You must use the [Star Wars API](https://swapi-api.alx-tools.com/)
+- You must use the `request` module
+
+### Install request module and use it
+[Documentantion](https://github.com/request/request)
+```bash
+sudo npm install request --global
+export NODE_PATH=/usr/lib/node_modules
+```
+
